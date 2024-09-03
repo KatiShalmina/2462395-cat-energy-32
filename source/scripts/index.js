7e12347d83b06234f90e8b7884e8448138f879a5
KatiShalmina <e.shalmina@gmail.com>
2024-09-03 23:30:42 +0200

    Добавляет декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-header__navigation-toggle');
+
+navMain.classList.remove('main-nav__list--nojs');
+navMain.classList.remove('main-header__navigation-toggle--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
