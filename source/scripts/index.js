6286405ddc390abd60f9a2a7c5f491ef5bb9972c
KatiShalmina <e.shalmina@gmail.com>
2024-09-10 19:13:15 +0200

    Вносит правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,14 +3,18 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-header__navigation-toggle');
 
 navMain.classList.remove('main-nav__list--nojs');
-navMain.classList.remove('main-header__navigation-toggle--nojs');
+navToggle.classList.remove('main-header__navigation-toggle--nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__list--open')) {
+    navMain.classList.remove('main-nav__list--open');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__list--open');
+  }
+
+  if (navToggle.classList.contains('burger-button--close')) {
+    navToggle.classList.remove('burger-button--close');
+  } else {
+    navToggle.classList.add('burger-button--close');
   }
 });
