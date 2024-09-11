d3516a1604c820cf1464c5febc56887178a11356
KatiShalmina <e.shalmina@gmail.com>
2024-09-11 23:47:49 +0200

    Вносит правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,22 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-header__navigation-toggle');
+const mainNavList = document.querySelector('.main-nav__list');
+const mainHeaderToggle = document.querySelector('.main-header__navigation-toggle');
 
-navMain.classList.remove('main-nav__list--nojs');
-navToggle.classList.remove('main-header__navigation-toggle--nojs');
+mainNavList.classList.remove('main-nav__list--nojs');
+mainHeaderToggle.classList.remove('main-header__navigation-toggle--nojs');
 
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav__list--open') && navToggle.classList.contains('burger-button--close')) {
-    navMain.classList.remove('main-nav__list--open');
-    navToggle.classList.remove('burger-button--close');
-  } else {
-    navMain.classList.add('main-nav__list--open');
-    navToggle.classList.add('burger-button--close');
-  }
+// mainHeaderToggle.addEventListener('click', () => {
+//   if (mainNavList.classList.contains('main-nav__list--open') && mainHeaderToggle.classList.contains('burger-button--close')) {
+//     mainNavList.classList.remove('main-nav__list--open');
+//     mainHeaderToggle.classList.remove('burger-button--close');
+//   } else {
+//     mainNavList.classList.add('main-nav__list--open');
+//     mainHeaderToggle.classList.add('burger-button--close');
+//   }
+// });
+
+mainHeaderToggle.addEventListener('click', () => {
+  mainHeaderToggle.toggle('burger-button--close');
+  mainNavList.toggle('main-nav__list--open');
 });
+
